78667bb32168554576bbb94ca31e143cc6f2e4c3
DMawi17 <asegda17@gmail.com>
2021-06-07 15:10:54 +0200

    all tasks are ready for review

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 // 1 Write a JavaScript function to truncate a string to a certain number of words.
+let str1 = "The quick brown fox jumps over the lazy dog";
+
+function truncate(str, maxLength = 7) {
+    let strToArray = str.split(" ");
+    return strToArray.length > maxLength
+        ? strToArray.slice(0, maxLength).join(" ") + `…`
+        : str;
+}
+
+console.log(truncate(str1)); // => "The quick brown fox jumps over the lazy…"
+console.log(truncate(str1, 3)); // => "The quick brown…"
+console.log(truncate(str1, 5)); // => "The quick brown fox jumps…"
 
 // 2 Write a JavaScript function to alphabetize a given string.
 
 // 3 Write a JavaScript function to convert ASCII to Hexadecimal format.
+
+function ascii_to_hexa(str) {
+    let arrStorage = [];
+    for (let i = 0; i < str.length; i++) {
+        const hexa = +str.codePointAt(i).toString(16);
+        arrStorage.push(hexa);
+    }
+    return arrStorage.join("");
+}
+
+console.log(ascii_to_hexa("12")); //3132
+console.log(ascii_to_hexa("100")); //313030
 
 // 4 Write a JavaScript function to get humanized number with the correct suffix such as 1st, 2nd, 3rd or 4th.r
 
@@ -11,6 +35,25 @@
 // Object
 
 // 6 Write a JavaScript function to sort the following array of objects by title value.
+
+var library = [
+    { author: "Bill Gates", title: "The Road Ahead", libraryID: 1254 },
+    { author: "Steve Jobs", title: "Walter Isaacson", libraryID: 4264 },
+    {
+        author: "Suzanne Collins",
+        title: "Mockingjay: The Final Book of The Hunger Games",
+        libraryID: 3245,
+    },
+];
+
+const sorted = (obj) => {
+    return obj.sort((a, b) => {
+        if (a.title < b.title) return -1;
+        if (a.title > b.title) return 1;
+    });
+};
+
+console.log(sorted(library));
 
 // 7 Write a JavaScript function to fill an array with values (numeric, string with one character) on supplied bounds.
 
