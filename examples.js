da0258f200ea46d74d346994d9f2523264c17733
André <andreglock@gmail.com>
2021-06-08 10:29:41 +0200

    examples

--- examples.js
+++ examples.js
@@ -1,4 +1,7 @@
 //import functions from index
+import { alphabetize_string } from index.js
+import { truncate } from index.js
+
 console.log(truncate(str1)); // => "The quick brown fox jumps over the lazy…"
 console.log(truncate(str1, 3)); // => "The quick brown…"
 console.log(truncate(str1, 5)); // => "The quick brown fox jumps…"
