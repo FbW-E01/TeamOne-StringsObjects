7fc1b8322f2ab3b2f5bccf19ebcf81e8a88c3eec
André <andreglock@gmail.com>
2021-06-08 14:33:16 +0200

    fixed successor

--- String.mjs
+++ String.mjs
@@ -3,105 +3,112 @@ const str1 = "The quick brown fox jumps over the lazy dog";
 const str2 = "Hello World";
 
 export function truncate(str, maxLength = 7) {
-    let strToArray = str.split(" ");
-    return strToArray.length > maxLength
-        ? strToArray.slice(0, maxLength).join(" ") + `…`
-        : str;
+  let strToArray = str.split(" ");
+  return strToArray.length > maxLength
+    ? strToArray.slice(0, maxLength).join(" ") + `…`
+    : str;
 }
 
 // 2 Write a JavaScript function to alphabetize a given string.
 export function alphabetize_string(string) {
-    let lettersArray = string.split(/['' ' ']*/);
-    lettersArray.sort(function (a, b) {
-        if (a.toLowerCase() < b.toLowerCase()) {
-            return -1;
-        }
-        if (a.toLowerCase() > b.toLowerCase()) {
-            return 1;
-        }
-    });
-    return lettersArray.join("");
+  let lettersArray = string.split(/['' ' ']*/);
+  lettersArray.sort(function (a, b) {
+    if (a.toLowerCase() < b.toLowerCase()) {
+      return -1;
+    }
+    if (a.toLowerCase() > b.toLowerCase()) {
+      return 1;
+    }
+  });
+  return lettersArray.join("");
 }
 
 // 3 Write a JavaScript function to convert ASCII to Hexadecimal format.
 
 export function ascii_to_hex(str) {
-    let arrStorage = [];
-    for (let i = 0; i < str.length; i++) {
-        // codePointAt(0): returns the UTF16 code for the characters at position 0
-        const hex = Number(str.codePointAt(i)).toString(16);
-        arrStorage.push(hex);
-    }
-    return arrStorage.join("");
+  let arrStorage = [];
+  for (let i = 0; i < str.length; i++) {
+    // codePointAt(0): returns the UTF16 code for the characters at position 0
+    const hex = Number(str.codePointAt(i)).toString(16);
+    arrStorage.push(hex);
+  }
+  return arrStorage.join("");
 }
 
 // 4 Write a JavaScript function to get humanized number with the correct suffix such as 1st, 2nd, 3rd or 4th.r
 export function humanize(number) {
-    if (number % 1 !== 0 || typeof number !== "number") {
-        return "Invalid input";
-    }
-    switch (number.toFixed().slice(-1)) {
-        case "1":
-            return `${number}st`;
-        case "2":
-            return `${number}nd`;
-        case "3":
-            return `${number}rd`;
-        default:
-            return `${number}th`;
-    }
+  if (number % 1 !== 0 || typeof number !== "number") {
+    return "Invalid input";
+  }
+  switch (number.toFixed().slice(-1)) {
+    case "1":
+      return `${number}st`;
+    case "2":
+      return `${number}nd`;
+    case "3":
+      return `${number}rd`;
+    default:
+      return `${number}th`;
+  }
 }
 
 // 5 Write a JavaScript function to get the successor of a string.
 
 export function successor(str) {
-    let alphabet = "abcdefghijklmnopqrstuvwxyz",
-        length = alphabet.length,
-        result = str,
-        i = str.length;
+  let alphabet = "abcdefghijklmnopqrstuvwxyz",
+    length = alphabet.length,
+    result = str,
+    i = str.length;
 
-    while (i >= 0) {
-        let last = str.charAt(--i),
-          next = "", index,
-            carry = false;
+  while (i > 0) {
+    let last = str.charAt(--i),
+      next = " ",
+      index,
+      carry = false;
 
-        if (isNaN(last)) {
-            index = alphabet.indexOf(last.toLowerCase());
+    if (isNaN(last)) {
+      index = alphabet.indexOf(last.toLowerCase());
 
-            if (index === -1) {
-                next = last;
-                carry = true;
-            } else {
-                let isUpperCase = last === last.toUpperCase();
-                next = alphabet.charAt((index + 1) % length);
-                if (isUpperCase) {
-                    next = next.toUpperCase();
-                }
-
-                carry = index + 1 >= length;
-                if (carry && i === 0) {
-                    let added = isUpperCase ? "A" : "a";
-                    result = added + next + result.slice(1);
-                    break;
-                }
-            }
-        } else {
-            next = +last + 1;
-            if (next > 9) {
-                next = 0;
-                carry = true;
-            }
-
-            if (carry && i === 0) {
-                result = "1" + next + result.slice(1);
-                break;
-            }
+      if (index === -1) {
+        next = last;
+        carry = true;
+      } else {
+        let isUpperCase = last === last.toUpperCase();
+        next = alphabet.charAt((index + 1) % length);
+        if (isUpperCase) {
+          next = next.toUpperCase();
         }
 
-        result = result.slice(0, i) + next + result.slice(i + 1);
-        if (!carry) {
-            break;
+        carry = index + 1 >= length;
+        if (carry && i === 0) {
+          let added = isUpperCase ? "A" : "a";
+          result = added + next + result.slice(1);
+          break;
         }
+      }
+    } else if (last === " ") {
+      //console.log('last is " "');
+      next = last;
+      carry = true;
+    } else {
+      //console.log('it happened', `"${last}"`);
+      next = +last + 1;
+      if (next > 9) {
+        next = 0;
+        carry = true;
+      }
+
+      if (carry && i === 0) {
+        result = "1" + next + result.slice(1);
+        break;
+      }
     }
-    return result;
+
+    result = result.slice(0, i) + next + result.slice(i + 1);
+
+    if (!carry) {
+      break;
+    }
+  }
+  return result;
 }
--- try.js
+++ try.js
@@ -1,20 +1,17 @@
 // 5 Write a JavaScript function to get the successor of a string.
 
-function successor(str) {
+export function successor(str) {
     let alphabet = "abcdefghijklmnopqrstuvwxyz",
         length = alphabet.length,
         result = str,
         i = str.length;
 
-  while (i >= 0) {
+  while (i > 0) {
     let last = str.charAt(--i),
-      next = " ",
+      next = ' ',
       index,
       carry = false;
-      console.log(last);
     
-    // console.log(isNaN(last));
-    // console.log(isNaN(last) || last === " ");
     if (isNaN(last)) {
       index = alphabet.indexOf(last.toLowerCase());
 
@@ -35,10 +32,12 @@ function successor(str) {
           break;
         }
       }
-    } else if (last === ' ') {
-      carry = true
-    }
-    else /* (last !== " ") */ {
+      } else if (last === ' ') {
+        //console.log('last is " "');
+        next = last;
+        carry = true;
+      } else {
+        //console.log('it happened', `"${last}"`);
             next = +last + 1;
             if (next > 9) {
                 next = 0;
@@ -50,21 +49,25 @@ function successor(str) {
               break;
           
             }
-        }
+      }
 
-        result = result.slice(0, i) + next + result.slice(i + 1);
-        if (!carry) {
-            break;
-        }
+    result = result.slice(0, i) + next + result.slice(i + 1);
+    
+    if (!carry) {
+        break;
+    }
+
     }
     return result;
 }
 
 
-// console.log(successor("abcd")); // "abce"
-// console.log(successor("THX1138")); // "THX1139"
-console.log(successor("<>")); // "< >"
-// console.log(successor("1999zzz ")); // "2000aaa"
-// console.log(successor("ZZZ9999 ")); // "AAAA0000"
-// console.log(successor("Z")); // "AAAA0000"
-// console.log(successor(" ")); // "AAAA0000"
+console.log(successor("abcd")); // "abce"
+console.log(successor("THX1138")); // "THX1139"
+console.log(successor("< >")); // "< >"
+console.log(successor("<>")); // "<1<>"
+console.log(successor("1999zzz ")); // "2000aaa "
+console.log(successor("ZZZ9999 ")); // "AAAA0000 "
+console.log(successor("ZZZ9999")); // "AAAA0000"
+console.log(successor("Z")); // "AA"
+console.log(successor(" ")); // "1"
