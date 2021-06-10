212bd470beb0c36ae5dc97d4d6adbf358d86042c
DMawi17 <asegda17@gmail.com>
2021-06-10 15:31:39 +0200

    sentences gererator is added

--- newCripto.js
+++ newCripto.js
@@ -1,35 +1,59 @@
 
-function cryptoTwo(text, diSypher = false) {
-    const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz,.!?";
-    const code = "ሀለሐመሠረሰሸቀበቨቯተቷቸቿኀነኗኘአከኸወዐዘዟዠዧየደጀገጠጨጰጸፀፈፐኍኟኧኵዷጇጧጯጷጿ ፥።፤?!";
+function sentenceGenerator(wordCount) {
+    wordGenerator = () => {
+        // generate random number b/n 2 and 13.
+        const randomNum = Math.floor(Math.random() * 11) + 2;
 
-    let empty = "";
-    for (let i = 0; i < text.length; i++) {
-        diSypher
-            ? (empty += alpha[code.indexOf(text[i])])
-            : (empty += code[alpha.indexOf(text[i])]);
+        // generate an array with random length.
+        const randomLength = Array(randomNum);
+
+        // fill the array with random number b/n 97 and 122
+        const ArrOfNum = randomLength
+            .fill()
+            .map(() => Math.floor(Math.random() * 26) + 97);
+
+        // converts numbers to letters
+        const ArrOfLetters = ArrOfNum.map((a) => String.fromCodePoint(a));
+
+        // console.log(ArrOfNum);
+        let vowels = ["a", "e", "o", "i", "u"];
+        let rand = Math.floor(Math.random() * 5);
+        const randomVowels = vowels[rand];
+
+        // check if there is a vowel if not add one at random
+        ArrOfLetters.map((l) =>
+            /[aeiou]/.test(l) ? l : ArrOfLetters.splice(rand, 1, randomVowels)
+        );
+
+        return ArrOfLetters.join("");
+    };
+
+    let arrHolder = [];
+    // loop through the wordCount and push words into the arrHolder
+    for (let i = 0; i < wordCount; i++) {
+        arrHolder.push(wordGenerator());
+    }
+    // generate sentences randomly
+    for (
+        let i = Math.floor(Math.random() * 10) + 5, counter = 1;
+        i < arrHolder.length - 1;
+        i += Math.floor(Math.random() * 10) + 5, counter++
+    ) {
+        arrHolder[i] = arrHolder[i] + ".";
+        arrHolder[i + 1] =
+            arrHolder[i + 1][0].toUpperCase() + arrHolder[i + 1].slice(1);
+        if (counter % 5 === 0) {
+            arrHolder.splice(i + 1, 0, "\n   ");
+        }
     }
 
-    return empty;
+    const stringHolder = arrHolder.join(" ");
+
+    return (
+        stringHolder[0].toUpperCase() +
+        stringHolder.slice(1, stringHolder.length - 1) +
+        "."
+    );
 }
 
-console.log(cryptoTwo("Hello world"));
-console.log(cryptoTwo("ሸጀፈፈኟዟጿኟዷፈደ", true));
-
-
-function wordGenerator() {
-    // generate random number b/n 2 and 13.
-    const randomNum = Math.floor(Math.random() * 11) + 1;
-
-    // generate random element in arr
-    const RandomLength = (rl = Array(randomNum));
-
-    // fill the array with random number of UTF-16
-    const ArrOfNum = rl.fill().map(() => Math.floor(Math.random() * 26) + 97);
-
-    // converts the UTF to letters.
-    const ArrOfLetters = ArrOfNum.map((a) => String.fromCodePoint(a));
-
-    // join the as a string
-    return ArrOfLetters.join("");
-}
+console.log(sentenceGenerator(100));
