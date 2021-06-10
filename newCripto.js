a7c007f38d12f7c37fe55c00320233dde248aedf
DMawi17 <asegda17@gmail.com>
2021-06-10 11:00:43 +0200

    Amharic addd

--- newCripto.js
+++ newCripto.js
@@ -1,11 +1,19 @@
-let text = 'Sample text';
-const i = 1;
-
-const alpha = 'ABCDEFGHIJKLMNOPQRSTUWXYZ abcdefghijklmnopqrstuvwxyz,.!?'
-const code = 'አማርኛአማርኛአማርኛአማርኛአማርኛአማርኛአማርኛአማርኛአማርኛአማርኛአማርኛአማርኛ'
+const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz,.!?";
+const code = "ሀለሐመሠረሰሸቀበቨቯተቷቸቿኀነኗኘአከኸወዐዘዟዠዧየደጀገጠጨጰጸፀፈፐኍኟኧኵዷጇጧጯጷጿ ፥።፤?!";
 
 // alpha.indexOf() char in alpha => index of char in code
 
-console.log(text[i]);
-console.log(alpha.indexOf(text[i]));
-console.log(code[alpha.indexOf(text[i])]);
+// console.log(text[i]);
+// console.log(alpha.indexOf(text[i]));
+// console.log(code[alpha.indexOf(text[i])]);
+
+// console.log(alpha.length);
+
+let text = "";
+
+let empty = '';
+for (let i = 0; i < text.length; i++) {
+      // empty += code[alpha.indexOf(text[i])]
+    empty += alpha[code.indexOf(text[i])];
+}
+console.log(empty);
