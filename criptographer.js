6f57e25437ba6b633e034d475742bf213c728dae
André <andreglock@gmail.com>
2021-06-10 10:58:05 +0200

    last cryptoOne before formating

--- criptographer.js
+++ criptographer.js
@@ -1,17 +1,50 @@
-function cryptography (text, key) {
+function cryptoOne (text, key) {
     let crypto = "";
     for (let i = 0; i < text.length; i++) {
+        // Get UTF-16 code for char
         let newChar = text.codePointAt(i);
-        if (newChar >= 32 && newChar <= )
-
-
+        // Lower case char
+        if (newChar >= 97 && newChar <= 122) {
+            newChar = newChar + key;
+            while (newChar > 122) {
+                newChar = newChar - 26;
+            } 
+            while (newChar < 97) {
+                newChar = newChar + 26;
+            }
+        }
+        // Upper case char
+        if (newChar >= 65 && newChar <= 90) {
+            newChar = newChar + key;
+            while (newChar > 90) {
+                newChar = newChar - 26;
+            }
+            while (newChar < 65) {
+                newChar = newChar + 26;
+            }
+        }
+        // Char with accent
+        if (newChar >= 192 && newChar <= 687) {
+            newChar = newChar + key;
+            while (newChar < 192) {
+                newChar = newChar - 496;
+            }
+            while (newChar > 687) {
+                newChar = newChar + 496;
+            }
+        }
         newChar = String.fromCodePoint(newChar);
-        
-        //console.log(text.codePointAt(i));
         crypto = crypto + newChar;
     }
     return crypto;
 }
 
-console.log(cryptography("az AZ", 1));
-
+console.log(cryptoOne("Beta abcde", 1)); // Cfub bcdef
+console.log(cryptoOne("Cfub bcdef", -1)); // Beta abcde
+console.log(cryptoOne("Beta abcde", 52)); // Beta abcde
+console.log(cryptoOne("André Lorenzoni", 102)); // Ylbpŏ Jmpclxmlg
+console.log(cryptoOne("Ylbpŏ Jmpclxmlg", -102)); // André Lorenzoni
+console.log(cryptoOne("íslenska, Móðir ekki veit ég það", 4)); // ñwpirwoe, Q÷ômv ioom zimx ík Ăeô
+console.log(cryptoOne("ñwpirwoe, Q÷ômv ioom zimx ík Ăeô", -4)); // íslenska, Móðir ekki veit ég það
+console.log(cryptoOne("Yinelea macı, sürekli dağılımı sağlayarak.", -40)); // Kuzqxqm ymoĉ, eÔdqwxu pm÷ĉxĉyĉ em÷xmkmdmw.
+console.log(cryptoOne("Kuzqxqm ymoĉ, eÔdqwxu pm÷ĉxĉyĉ em÷xmkmdmw.", 40)); // Yinelea macı, sürekli dağılımı sağlayarak.
