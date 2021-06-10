7a2f319ea05097d4299e51991694397c57dbcc61
DMawi17 <asegda17@gmail.com>
2021-06-10 11:33:13 +0200

    Lorem Project Trail

--- newCripto.js
+++ newCripto.js
@@ -1,19 +1,35 @@
-const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz,.!?";
-const code = "ሀለሐመሠረሰሸቀበቨቯተቷቸቿኀነኗኘአከኸወዐዘዟዠዧየደጀገጠጨጰጸፀፈፐኍኟኧኵዷጇጧጯጷጿ ፥።፤?!";
 
-// alpha.indexOf() char in alpha => index of char in code
+function cryptoTwo(text, diSypher = false) {
+    const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz,.!?";
+    const code = "ሀለሐመሠረሰሸቀበቨቯተቷቸቿኀነኗኘአከኸወዐዘዟዠዧየደጀገጠጨጰጸፀፈፐኍኟኧኵዷጇጧጯጷጿ ፥።፤?!";
 
-// console.log(text[i]);
-// console.log(alpha.indexOf(text[i]));
-// console.log(code[alpha.indexOf(text[i])]);
+    let empty = "";
+    for (let i = 0; i < text.length; i++) {
+        diSypher
+            ? (empty += alpha[code.indexOf(text[i])])
+            : (empty += code[alpha.indexOf(text[i])]);
+    }
 
-// console.log(alpha.length);
-
-let text = "";
-
-let empty = '';
-for (let i = 0; i < text.length; i++) {
-      // empty += code[alpha.indexOf(text[i])]
-    empty += alpha[code.indexOf(text[i])];
+    return empty;
 }
-console.log(empty);
+
+console.log(cryptoTwo("Hello world"));
+console.log(cryptoTwo("ሸጀፈፈኟዟጿኟዷፈደ", true));
+
+
+function wordGenerator() {
+    // generate random number b/n 2 and 13.
+    const randomNum = Math.floor(Math.random() * 11) + 1;
+
+    // generate random element in arr
+    const RandomLength = (rl = Array(randomNum));
+
+    // fill the array with random number of UTF-16
+    const ArrOfNum = rl.fill().map(() => Math.floor(Math.random() * 26) + 97);
+
+    // converts the UTF to letters.
+    const ArrOfLetters = ArrOfNum.map((a) => String.fromCodePoint(a));
+
+    // join the as a string
+    return ArrOfLetters.join("");
+}
